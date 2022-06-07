5b67b1b82e2be42a13c05edb6d6b8c1401fc00cd
coolmarvel <marvel97@naver.com>
2022-06-07 14:34:15 +0900

    문자열 결합

---
+++ sec3/StringDemo4.java
@@ -0,0 +1,10 @@
+package sec3;
+
+public class StringDemo4 {
+    public static void main(String[] args) {
+        int i = 7;
+        System.out.println("Java" + i);
+        System.out.println("Java" + 7);
+        System.out.println(7 + 1 + "Java" + 7 + 1);
+    }
+}

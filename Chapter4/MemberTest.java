19280c2e8b222d456663f3cc7ad00bec6e1d16e8
coolmarvel <marvel97@naver.com>
2022-06-07 13:33:58 +0900

    MemberTest

---
+++ Chapter4/MemberTest.java
@@ -0,0 +1,54 @@
+package Chapter4;
+
+class Member {
+    // 회원정보
+    private String name;
+    private String id;
+    private String password;
+    private int age;
+
+    // 생성자
+    public Member(String name, String id, String password, int age) {
+        this.name = name;
+        this.id = id;
+        this.password = password;
+        this.age = age;
+    }
+
+    // get = 접근자, set = 설정자
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+
+public class MemberTest {
+
+}

d6cf55791adb4b29acd2b1c647bc52a6d770ccbf
coolmarvel <marvel97@naver.com>
2022-06-07 09:41:19 +0900

    CircleDemo 합침

--- sec1/CircleDemo.java
+++ sec1/CircleDemo.java
@@ -1,25 +1,102 @@
 package sec1;
 
 class Circle {
-    // 클래스 외부의 멤버 접근
-    double radius;
+    // private double radius;
 
-    double findArea() {
-        return 3.14 * radius * radius;
-    }
+    // // 접근자
+    // public double getRadius() {
+    // return radius;
+    // }
 
-    void show(double x, double y) {
-        System.out.printf("반지름 = %.1f, 넓이 = %.1f\n", x, y);
-    }
+    // // 설정자
+    // public void setRadius(double r) {
+    // this.radius = r;
+    // }
+
+    // double findArea() {
+    // return 3.14 * radius * radius;
+    // }
+
+    // void show(double x, double y) {
+    // System.out.printf("반지름 = %.1f, 넓이 = %.1f\n", x, y);
+    // }
+
+    // private double radius;
+
+    // public Circle(double r) {
+    // radius = r;
+    // }
+
+    // double radius;
+    // String color;
+
+    // // 생성자1 (임의의 반지름과 색상을 가진 객체 생성자)
+    // public Circle(double radius, String color) {
+    // radius = radius;
+    // color = color;
+    // }
+
+    // // 생성자2 (파랑 객체 생성자)
+    // public Circle(double radius) {
+    // this.radius = radius;
+    // this.color = "파랑";
+    // }
+
+    // // 생성자3 (반지름이 10.0인 객체 생성자)
+    // public Circle(String color) {
+    // this.radius = 10.0;
+    // this.color = color;
+    // }
+
+    // // 생성자4 (반지름이 10.0인 빨강 객체 생성자)
+    // public Circle() {
+    // this.radius = 10.0;
+    // this.color = "빨강";
+    // }
 }
 
 public class CircleDemo {
+    // 클래스 내부의 멤버 접근
+    // static double radius;
+
+    // static double findArea() {
+    // return 3.14 * radius * radius;
+    // }
+
+    // static void show(double x, double y) {
+    // System.out.printf("반지름 = %.1f, 넓이 = %.1f\n", x + y);
+    // }
+
     public static void main(String[] args) {
-        // 클래스 외부의 멤버 접근
-        Circle mycCircle = new Circle();
+        // radius = 10.0;
 
-        mycCircle.radius = 10.0;
+        // double area = findArea();
 
-        mycCircle.show(mycCircle.radius, mycCircle.findArea());
+        // show(radius, area);
+
+        // Circle myCircle = new Circle();
+
+        // myCircle.setRadius(10.0);
+
+        // myCircle.show(myCircle.getRadius(), myCircle.findArea());
+
+        // Circle mycCircle = new Circle(10.0);
+
+        // // 생성자1 호출
+        // Circle c1 = new Circle(10.0, "빨강");
+
+        // // 생성자2 호출
+        // Circle c2 = new Circle(10.0);
+
+        // // 생성자3 호출
+        // Circle c3 = new Circle("노랑");
+
+        // // 생성자4 호출
+        // Circle c4 = new Circle();
+
+        // System.out.println(c1);
+        // System.out.println(c2);
+        // System.out.println(c3);
+        // System.out.println(c4);
     }
 }

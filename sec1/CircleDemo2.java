622cc2058cae70bd34710fba39d3bafe4029de7a
coolmarvel <marvel97@naver.com>
2022-06-07 09:18:29 +0900

    생성자를 포함한 클래스

---
+++ sec1/CircleDemo2.java
@@ -0,0 +1,15 @@
+package sec1;
+
+class Circle {
+    private double radius;
+
+    public Circle(double r) {
+        radius = r;
+    }
+}
+
+public class CircleDemo2 {
+    public static void main(String[] args) {
+        Circle mycCircle = new Circle(10.0);
+    }
+}

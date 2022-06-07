4ff9f612b43a941c9803872de64ddc112997c8bf
coolmarvel <marvel97@naver.com>
2022-06-07 14:50:22 +0900

    ArrayList를 이용한 데이터의 평균

--- sec3/ArrayListDemo.java
+++ sec3/ArrayListDemo.java
@@ -1,7 +1,26 @@
 package sec3;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class ArrayListDemo {
     public static void main(String[] args) {
-        
+        Scanner in = new Scanner(System.in);
+        ArrayList<Integer> scores = new ArrayList<>();
+
+        int data;
+        int sum = 0;
+
+        System.out.println("-1을 입력하시기 전까지의 입력값의 평균을 구합니다.");
+
+        while ((data = in.nextInt()) >= 0) {
+            scores.add(data);
+        }
+
+        for (int i = 0; i < scores.size(); i++) {
+            sum += scores.get(i);
+        }
+
+        System.out.println("평균 = " + sum / scores.size());
     }
 }

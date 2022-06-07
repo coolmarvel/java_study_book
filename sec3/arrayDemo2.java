330043868cc71984c1aae782aa2e0359971f51ee
coolmarvel <marvel97@naver.com>
2022-06-07 14:46:15 +0900

    평균 이자율 계산

--- sec3/arrayDemo2.java
+++ sec3/arrayDemo2.java
@@ -1,6 +1,6 @@
 package sec3;
 
-public class arrayDemo2 {
+public class ArrayDemo2 {
 
     public static void main(String[] args) {
         double[][] interests = { { 3.2, 3.1, 3.2, 3.0 }, { 2.9, 2.8, 2.7, 2.6 }, { 2.7, 2.6, 2.5, 2.7 } };

95f7cc9cba2581cb026112725eb2c8e56f30c76f
coolmarvel <marvel97@naver.com>
2022-06-07 15:11:37 +0900

    가변 길이 인수 활용

---
+++ sec3/VarArgsDemo.java
@@ -0,0 +1,16 @@
+package sec3;
+
+public class VarArgsDemo {
+    public static void main(String[] args) {
+        printSum(1, 2, 3, 4, 5);
+        printSum(10, 20, 30);
+    }
+
+    public static void printSum(int... v) {
+        int sum = 0;
+        for (int i : v) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}

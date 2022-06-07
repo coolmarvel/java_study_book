6274d8c7859cbc13215ba50fd54ceb0d976dadf9
coolmarvel <marvel97@naver.com>
2022-06-07 15:39:41 +0900

    세 종류의 에러 발생

--- sec3/DebugDemo1.java
+++ sec3/DebugDemo1.java
@@ -2,11 +2,11 @@ package sec3;
 
 public class DebugDemo1 {
     public static void main(String[] args) {
-        int d, m, n, n2, n3;
-        d = 0;
-        n = 2;
-        n2 = n * n:
-        n3 = n2 * n2;
-        m = n / d;
+        // int d, m, n, n2, n3;
+        // d = 0;
+        // n = 2;
+        // n2 = n * n:
+        // n3 = n2 * n2;
+        // m = n / d;
     }
 }

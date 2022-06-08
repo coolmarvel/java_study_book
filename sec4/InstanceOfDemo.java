d748d0ca566074993af0eebc56e3ccb435af7bc8
coolmarvel <marvel97@naver.com>
2022-06-08 13:12:23 +0900

    instanceof 연산자의 활용

---
+++ sec4/InstanceOfDemo.java
@@ -0,0 +1,21 @@
+package sec4;
+
+public class InstanceOfDemo {
+    public static void main(String[] args) {
+        Student s = new Student();
+        Person p = new Person();
+
+        System.out.println(s instanceof Person);
+        System.out.println(s instanceof Student);
+        System.out.println(p instanceof Student);
+
+        downcast(s);
+    }
+
+    static void downcast(Person p) {
+        if (p instanceof Student) {
+            Student s = (Student) p;
+            System.out.println("오케이, 하향 타입 변환");
+        }
+    }
+}

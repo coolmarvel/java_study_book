a5affa2916d4c88c27a061fec47311ed55d66d30
coolmarvel <marvel97@naver.com>
2022-06-08 12:56:59 +0900

    final class 활용

---
+++ sec3/FinalClassDemo.java
@@ -0,0 +1,20 @@
+package sec3;
+
+class Good {
+
+}
+
+class Better extends Good {
+
+}
+
+final class Best extends Better {
+
+}
+
+public class FinalClassDemo {
+    public static void main(String[] args) {
+        // new NumberOne();
+        new Best();
+    }
+}

36ad68fed7b7522b6528dcfeb5a3e0b7e1ea96f6
coolmarvel <marvel97@naver.com>
2022-06-10 13:38:52 +0900

    인터페이스의 활용

---
+++ sec9/ListDemo.java
@@ -0,0 +1,25 @@
+package sec9;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListDemo {
+    public static void main(String[] args) {
+        // String[] names1 = { "사슴", "호랑이", "바다표범", "곰" };
+
+        // List<String> list = Arrays.asList(names1);
+        // List.set(1, "앵무새");
+        // for (String string : list) {
+        // System.out.print(string + "\t");
+        // }
+
+        // System.out.println();
+
+        // list.sort((x, y) -> x.length() - y.length());
+        // String[] names2 = list.toArray(new String[List.size()]);
+        // for (int i = 0; i < names2.length; i++) {
+        // System.out.print(names2[i] + "\t");
+        // }
+    }
+
+}

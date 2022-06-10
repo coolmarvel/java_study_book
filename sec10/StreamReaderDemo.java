cbee23c11c6b6bc4229637d92a2c504bae6bba7e
coolmarvel <marvel97@naver.com>
2022-06-10 16:50:27 +0900

    InputStreamReader의 활용

---
+++ sec10/StreamReaderDemo.java
@@ -0,0 +1,23 @@
+package sec10;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StreamReaderDemo {
+    public static void main(String[] args) {
+        String input = "D:\\Temp\\org.txt";
+
+        try {
+            FileInputStream fi = new FileInputStream(input);
+            InputStreamReader in = new InputStreamReader(fi, "US-ASCII");
+            int c;
+            while ((c = in.read()) != -1) {
+
+                System.out.println((char) c);
+            }
+        } catch (IOException e) {
+            System.out.println("Error!");
+        }
+    }
+}

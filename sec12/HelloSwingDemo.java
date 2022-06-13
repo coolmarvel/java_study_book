e1e73a3fd15354a5a49df6b070e134ede1c8db66
coolmarvel <marvel97@naver.com>
2022-06-13 11:41:06 +0900

    스윙 기반의 맛보기 프로그램

---
+++ sec12/HelloSwingDemo.java
@@ -0,0 +1,13 @@
+package sec12;
+
+import javax.swing.JFrame;
+
+public class HelloSwingDemo {
+    public static void main(String[] args) {
+        JFrame jf = new JFrame();
+
+        jf.setTitle("안녕, 스윙!");
+        jf.setSize(300, 100);
+        jf.setVisible(true);
+    }
+}

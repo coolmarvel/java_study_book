d8f6ec3adb88c5c138c38fffda73a457f206229e
coolmarvel <marvel97@naver.com>
2022-06-13 13:00:55 +0900

    패널을 이용한 컴포넌트 추가

---
+++ JFrameDemo4.java
@@ -0,0 +1,24 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class JFrameDemo4 extends JFrame {
+    JFrameDemo4() {
+        setTitle("안녕, 스윙!");
+        JPanel jp = new JPanel();
+        JLabel jl = new JLabel();
+        JButton jb = new JButton();
+        jp.add(jl);
+        jp.add(jb);
+        add(jp);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 100);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new JFrameDemo4();
+    }
+}
